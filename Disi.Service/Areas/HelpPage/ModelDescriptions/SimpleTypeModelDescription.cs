37195e85b497dea221e6356afd0d4bf9899a7f7f
Nelson Valverde La Torre <nelsonvalverdelt@outlook.com>
2017-11-17 00:49:55 -0500

    Template Project Analysis  Disi (Digital Signature)

---
+++ Disi.Service/Areas/HelpPage/ModelDescriptions/SimpleTypeModelDescription.cs
@@ -0,0 +1,6 @@
+namespace Disi.Service.Areas.HelpPage.ModelDescriptions
+{
+    public class SimpleTypeModelDescription : ModelDescription
+    {
+    }
+}
---
+++ Disi.Service/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Disi.Service.WebApiApplication" Language="C#" %>
